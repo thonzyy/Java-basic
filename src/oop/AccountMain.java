d75421c1aac6c5923dba1784d62581763d34cbfd
thonzyy <136280932+thonzyy@users.noreply.github.com>
2024-05-20 14:17:33 +0900

    객체지향계좌 만들기
    int balance -> 잔액
    -deposit(int mount) : 입금 메소드
    입금 시 잔액 증가
    -withdraw(int amount) 출금 메소드
    출금 시 잔액 감소 
    
    - AccountMain 클래스를 만들고 main() 메소드를 통해 프로그램을 시작
      계좌에 10000원 입금
      계좌에 9000원 입금
      계좌에 2000원을 출금 시도 -> 잔액 부족 출력 확인
     잔고를 출력한다. -> 잔고 : 1000원
    
    데이터 묶기, 메소드 추출, 클래스와 메소드 
    객체지향프로그래밍

---
+++ src/oop/AccountMain.java
@@ -0,0 +1,14 @@
+package oop.ex;
+
+public class AccountMain
+{
+
+    public static void main(String[] args)
+    {
+        Account acc = new Account();
+        acc.deposit(10000);
+        acc.withdraw(9000);
+        acc.withdraw(2000);
+        System.out.println("잔고: "+acc.balance);
+    }
+}
